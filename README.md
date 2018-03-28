9c09177f1d9c63b4d7cd3cae325d294778f9f950
HMR <hmrserver@gmail.com>
2018-03-29 00:52:10 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# GBackup
+## GBackup
+![alt tag](https://raw.githubusercontent.com/hmrserver/GBackup/master/preview.png)
+
+## Introduction
+GBackup is an automated script which would compress user mentioned Mysql / mariaDB databases ( if mentioned ) and Server files into one file and upload it at Google Drive. It would also clear up the files from google drive which are n days old in the mentioned Folder Name (user can change the number of day(n) here).
+
+# What This Script Does
+--------------
+This script does some simple tasks:
+* The script Installs gdrive if it does not already installed.
+* The script Deletes Older files from the mentioned Folder (change DAYSKEEP @ script).
+* The script dumps all of your MySQL databases individually.
+* The script backs up all of your files (e.g: root of all of your virtual hosts or your important files).
+* The script compresses your files and databases to a single archive.
+* The script uploads the compressed archive into a folder in your Google Drive account.
+* After the upload, the script cleans up the temporary files (dumps, the archive itself locally).
+
+User can configure its settings which would be given at top portion of the script.
+
+### I have tested on Centos-6,7/Redhat-6,7.
+
+# Custom License
+--------------
+ * User may edit the item.
+ * User can't re-distribute the script copy for free or paid.
+ * Use it at your own risk. I'm not holding any responsibilities for any damage that this script may do (which shouldn't).
+
+# Requirements
+--------------
+* `wget` - To download the gdrive installer.
+* `mysql` (cli) - To list databases.
+* `mysqldump` - To dump databases (in most cases, it comes with `mysql` cli).
+* `nano` - To edit the script's Configurations manually.
+
+# Installation
+--------------
+If you have all the requirements installed.
+then Follow the below instructions:
+
+Go to a place where you want to place the script (for now lets say /root/).
+```
+cd /root/
+```
+
+Now Download the Bash script by:
+```
+wget https://raw.githubusercontent.com/hmrserver/GBackup/master/backup_gdrive.sh
+```
+
+Now Give appropriate permissions
+```
+chmod 600 backup_gdrive.sh
+```
+
+Now To edit the Script's Configurations:
+```
+nano backup_gdrive.sh
+```
+and edit the Top Part configurations (its well descripted in the script about each options).
+
+after that save it and try to run it first time manually(assuming /root/ was the folder where you came at first).
+```
+/root/backup_gdrive.sh
+```
+It would provide you a oauth link, copy and paste the link in your web browser, it will ask for you to login with your gdrive account, just do so then it will generate a key after login, copy and paste it to the terminal (where it was asking for that key).
+
+Now its been ready to be automated.
+
+Simply use it in a Cron and forget about it :D
+
+# For adding in Cron
+type this command to enter in the cron editor:
+```
+crontab -e
+```
+
+then press i to start the insert mode and paste this in the last of the editor:
+```
+00 01,13 * * * /root/backup_gdrive.sh >> /root/backup_log.log 2>&1
+```
+(replace /root/ with your firstly place where you came).
+and then save it by pressing esc then typing :wq and hitting enter.
+this cron will run the script twice everyday (at 1:00 & 13:00). and you can see the backup_log.log for crontab logs.
+For adjusting the cron according you your need, you can have a look at [here](https://docs.acquia.com/article/cron-time-string-format).
---
+++ backup_gdrive.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+
+#Please note this script,I have been tested on Centos-6,7/Redhat-6,7.
+
+
+######################################################################################################################################
+# Change the below mentioned server details to your own Server - Database Details, Backup Local Path, Remote Backup Path Name, And WeB Server FDirectory Path.
+######################################################################################################################################
+
+#Database credentials
+enable_db_backup="yes" # options: yes /  no, Choose whether you want's to backup databases also or not.
+database_user="root"  # feed your Database User
+database_password=' ' # feed your Database password if any
+database_host="localhost"
+database_port="3306"  #feed your Database PORT, leave like this if you don't know
+
+#Define local path for backups
+backup_tmp="/tmp/Server_Backup" # IMPORTANT: Only Put a Empty Folder Name (which would be used to store temp compressed files and all files would be deleted after backup!)
+
+#Define remote backup path (which would be seen at Google DRIVE)
+backup_folder="Files & MySQL Backup"
+
+#Define Prefix used before backup filenames
+PREFIX="Gbackup_" # feed the prefix which you wants to be shown at your Google Drive backuped comressed files.. leave empty if you don't want ant prefix.
+
+#Web Server Folder Path which needs to be backuped, you can use multiple paths seperated by spaces.
+file_paths="/var/www"
+
+#Days to retain - In the DAYSKEEP variable you can specify how many days of backups you would like to keep, any older ones will be deleted from Google Drive.
+#Set 0 for Not limiting backups
+DAYSKEEP=300
+
+#################################################################################################################################
+
+echo "---------------------------------------------------------"
+echo '    _____   ____                _      _    _  _____  '
+echo '   / ____| |  _ \              | |    | |  | ||  __ \ '
+echo '  | |  __  | |_) |  __ _   ___ | | __ | |  | || |__) |'
+echo '  | | |_ | |  _ <  / _` | / __|| |/ / | |  | ||  ___/ '
+echo '  | |__| | | |_) || (_| || (__ |   <  | |__| || |     '
+echo '   \_____| |____/  \__,_| \___||_|\_\  \____/ |_|     '
+echo '                                                      '
+echo "  Created by HMR (https://github.com/hmrserver)"
+echo "---------------------------------------------------------"
+echo " "
+
+
+
+#Check Internet Connection
+IS=`/bin/ping -c 5 4.2.2.2 | grep -c "64 bytes"`
+
+if (test "$IS" -gt "2") then
+        internet_conn="1"
+
+#Check File
+file="/usr/bin/gdrive"
+if [ -f "$file" ]
+then
+  echo " "
+  echo "Gdrive Found!"
+  echo " "
+else
+
+  echo "Gdrive Not Found!"
+  echo "Installing Gdrive....."
+  echo " "
+
+#Download And Install Gdrive
+if [ `getconf LONG_BIT` = "64" ]
+then
+        wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download" -O /usr/bin/gdrive
+        chmod 777 /usr/bin/gdrive
+	gdrive list
+	clean
+else
+        wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnLV92dHBpTkFhTEU&export=download" -O /usr/bin/gdrive
+        chmod 777 /usr/bin/gdrive
+	gdrive list
+	clean
+fi
+fi
+
+	echo "Backup Process Starting......................."
+  echo " "
+
+
+#Date prefix
+DATEFORM=$(date +"%d-%m-%Y")
+
+#Calculate days as filename prefix
+DAYSKEPT=$(date +"%Y-%m-%d" -d "-$DAYSKEEP days")
+
+#Make sure the backup folder exists
+mkdir -p $backup_tmp
+
+
+
+    ## Find Folder ID if exists
+  folderData=$(gdrive list -q "mimeType = 'application/vnd.google-apps.folder' AND name='$backup_folder' AND trashed=false" --no-header)
+
+  ## If folder does not exist create new folder.
+  if [[ -z "${folderData// }" ]]
+  then
+  	echo "$backup_folder Folder not found"
+      echo "Creating Folder: $backup_folder"
+  	echo " "
+      new_folder=$(gdrive mkdir --parent "$backup_folder")
+      new_folder=($new_folder)
+      folderID=${new_folder[1]}
+  else
+      folder_data=($folderData)
+      folderID=${folder_data[0]}
+
+      if [[ -n "${folderID// }" ]]
+      then
+          #execute if the the variable is not empty and contains non space characters
+          echo "FOLDER EXISTS - ID: $folderID"
+          echo " "
+      else
+          #execute if the variable is empty or contains only spaces
+          echo "ERROR CREATING FOLDER"
+          echo " "
+      fi
+
+  fi
+
+    if [ ! $DAYSKEEP=0 ]; then
+    #Delete old backup, get folder id and delete if exists
+    expired_backups=`gdrive list -m 1000 --query " ('$folderID' in parents) and trashed = false and modifiedTime < '$DAYSKEPT'" --order "modifiedTime asc" --no-header | cut -d ' ' -f1`;
+
+    # Deletes backups that are more than n days old.
+    if [[ -n $expired_backups ]]
+    then
+    echo " "
+    echo "-----------------------"
+    echo "Deleting FILES from $backup_folder, which are $DAYSKEEP days Older"
+    while read -r backup_id; do
+        echo Deleting $backup_id...
+        gdrive delete --recursive $backup_id
+    done <<< "$expired_backups"
+    echo "-----------------------"
+    echo " "
+    echo " "
+  fi
+
+fi
+
+    #Create the local backup folder if it doesn't exist
+    if [ ! -e $backup_tmp ]; then
+      echo " "
+      echo "Local Path For Backups Not Found!"
+      echo "creating one..."
+      echo " "
+        mkdir $backup_tmp
+    fi
+
+    if [ $enable_db_backup = "yes" ]; then
+      echo " "
+      echo "Backuping the DATABASES"
+      echo "-----------------------"
+        #Create the local mysql backup folder if it doesn't exist
+        if [ ! -e $backup_tmp/mysql ]; then
+          mkdir $backup_tmp/mysql
+        fi
+
+               # Let's start dumping the databases
+                 databases=$(mysql --host="$database_host" --port="$database_port" --user="$database_user" --password="$database_password" -e "SHOW DATABASES;" 2>&1 | grep -v "Warning: Using a password" | tr -d "| " | grep -v Database)
+                 for db in $databases; do
+                     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+                         echo "-> Dumping database: $db"
+                         mysqldump --compress --host="$database_host" --port="$database_port" --user="$database_user" --password="$database_password" --events $db > $backup_tmp/mysql/$db.sql 2>&1 | grep -v "Warning: Using a password"
+                     fi
+             	done
+
+    fi
+    echo "-----------------------"
+    echo " "
+
+    #Back up the Web folder
+    if [ ! -e $backup_tmp/files ]; then
+      mkdir $backup_tmp/files
+    fi
+    cd $backup_tmp
+    echo " "
+    echo "Compressing all files for Upload....."
+    if [ $enable_db_backup = "yes" ]; then
+       mysql="mysql"
+    else
+      mysql=""
+      fi
+    tar -czf $backup_tmp/$PREFIX$DATEFORM.tar.gz $mysql $file_paths
+    echo " "
+
+
+    #Upload Server Data & Mysql database tar
+    echo " "
+    echo "-----------------------"
+    echo "Uploading $PREFIX$DATEFORM.tar.gz at Google Drive..."
+    gdrive upload --parent -delete $folderID $PREFIX$DATEFORM.tar.gz
+    echo "-----------------------"
+
+    #Final Cleanup
+    echo " "
+    echo "Clearing Junk files..."
+    chmod -R 777 /tmp/*
+    echo "-> Removing $backup_tmp"
+    rm -rf $backup_tmp
+    echo "Done!"
+    echo " "
+
+    #Dispaly Internet Connection Error Message
+else
+   internet_conn="0"
+
+   echo "#################################### Please Check Your Internet Connection. #######################################"
+   echo "-------------------------------------------------------------------------------------------------------------------"
+fi
